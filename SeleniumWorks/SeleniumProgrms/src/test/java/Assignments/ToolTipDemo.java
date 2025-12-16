5c40a7bc443f33608d972d8efa9edd19b3232331
Karan A <karananbunidhi@gmail.com>
2025-12-16 22:22:16 +0530

    new files

---
+++ SeleniumWorks/SeleniumProgrms/src/test/java/Assignments/ToolTipDemo.java
@@ -0,0 +1,24 @@
+package Assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ToolTipDemo {
+
+    public static void main(String[] args) {
+    	WebDriver driver=new ChromeDriver();
+    	driver.manage().window().maximize();
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    	driver.get("https://www.globalsqa.com/demoSite/practice/tooltip/forms.html");
+    	
+    	WebElement firstName=driver.findElement(By.id("firstname"));
+    	
+    	String value=firstName.getAttribute("title");
+    	System.out.println(value);
+       
+    }
+}
